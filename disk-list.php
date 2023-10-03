1e885eb5d508e45d3061d83f23393e5a082ebe72
mauroluciano <mauroluciano911@gmail.com>
2023-10-03 13:08:34 +0200

    add isset

--- disk-list.php
+++ disk-list.php
@@ -1,3 +1,16 @@
 <?php
+$string = file_get_contents("..data/dischi.json");
+$diskList = json_decode($string, true);
+$result = [];
 
->
+if (isset($_GET['index'])) {
+    $diskIndex = 
+}
+
+
+
+
+
+
+
+?>
